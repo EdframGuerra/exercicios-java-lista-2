b74622943d4426e8bdd5fb86d284c71f426171a4
Edfram-Guerra <edfram.guerra@gmail.com>
2024-07-20 22:28:11 -0300

    Exercicio009 finalizado

---
+++ exercicio009/Farenheit.java
@@ -0,0 +1,22 @@
+package exercicio009;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Farenheit {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Informe a temperatura Farenheit: ");
+        double farenheit = leitor.nextDouble();
+
+        double celsius = (5*(farenheit-32)/9);
+
+        System.out.printf("%.2f", farenheit);
+        System.out.print(" graus = ");
+        System.out.print(celsius+ " graus");
+
+        leitor.close();
+    }
+}
