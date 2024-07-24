3ac72877f4cc8623e7653dc9d020bb6666c5b2cd
Edfram-Guerra <edfram.guerra@gmail.com>
2024-07-23 22:42:23 -0300

    Implementado tratamentopara as exceções

--- exercicio003/Soma.java
+++ exercicio003/Soma.java
@@ -1,21 +1,29 @@
 package exercicio003;
 
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Soma {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner leitor = new Scanner(System.in);
-        System.out.print("Digite um numumero: ");
-        double numero1 = leitor.nextDouble();
-        System.out.print("Digite outro numero: ");
-        double numero2 = leitor.nextDouble();
 
-        double soma = numero1 + numero2;
+        try {
+            Scanner leitor = new Scanner(System.in);
+            System.out.print("Digite um numumero: ");
+            double numero1 = leitor.nextDouble();
+            System.out.print("Digite outro numero: ");
+            double numero2 = leitor.nextDouble();
 
-        System.out.printf("A soma dos numeros digitados é %.0f" , soma);
+            double soma = numero1 + numero2;
 
-        leitor.close();
+            System.out.printf("A soma dos numeros digitados é %.2f", soma);
+
+            leitor.close();
+
+        } catch (InputMismatchException e) {
+            System.out.println("Número invalido");
+        }
+
     }
 }
