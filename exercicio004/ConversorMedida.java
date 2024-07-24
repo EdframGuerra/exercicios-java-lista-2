0ee64ae5c77d430fe5e839de3e650a8dfa2007ce
Edfram-Guerra <edfram.guerra@gmail.com>
2024-07-23 22:45:30 -0300

    System.out.println(Conversor de metros para centimetros);

--- exercicio004/ConversorMedida.java
+++ exercicio004/ConversorMedida.java
@@ -1,5 +1,6 @@
 package exercicio004;
 
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,17 +9,21 @@ public class ConversorMedida {
         Locale.setDefault(Locale.US);
         Scanner leitor = new Scanner(System.in);
 
-        System.out.println("Conversor de metros para centimetros");
-        System.out.print("Informe a quantidade de metros: ");
-        double metros = leitor.nextDouble();
+        try {
+            System.out.println("Conversor de metros para centimetros");
+            System.out.print("Informe a quantidade de metros: ");
+            double metros = leitor.nextDouble();
 
-        double centimetros = metros * 100;
+            double centimetros = metros * 100;
 
-        System.out.print(metros + " metros, corresponde a ");
-        System.out.printf("%.2f", centimetros);
-        System.out.println(" centimetros");
+            System.out.print(metros + " metros, corresponde a ");
+            System.out.printf("%.2f", centimetros);
+            System.out.println(" centimetros");
 
-        leitor.close();
+            leitor.close();
+        } catch (InputMismatchException e) {
+            System.out.println("Formato invalido");
+        }
 
     }
 }
