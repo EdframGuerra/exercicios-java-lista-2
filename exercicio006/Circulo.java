f5f6c58e96de0a6083d8d36f4fe6c47000b982ce
Edfram-Guerra <edfram.guerra@gmail.com>
2024-07-23 22:53:19 -0300

    Implementado tratamento de execeção

--- exercicio006/Circulo.java
+++ exercicio006/Circulo.java
@@ -2,6 +2,7 @@ package exercicio006;
 
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Circulo {
 
@@ -9,17 +10,21 @@ public class Circulo {
         Locale.setDefault(Locale.US);
         Scanner leitor = new Scanner(System.in);
 
-        double pi = 3.141592653;
-     
-    
-        System.out.print("Informe o raio: ");
-        double raio = leitor.nextDouble();
+        try {
+            double pi = 3.141592653;
 
-        double area = pi*(raio*raio);
+            System.out.print("Informe o raio: ");
+            double raio = leitor.nextDouble();
 
-        System.out.printf("A area do circulo é %.2f ",area);
-    
-        leitor.close();
+            double area = pi * (raio * raio);
+
+            System.out.printf("A area do circulo é %.2f ", area);
+
+            leitor.close();
+        } catch (InputMismatchException e) {
+            System.out.println("Número em formato invalido");
+        }
+
     }
-   
+
 }
