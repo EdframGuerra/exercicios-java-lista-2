bdd3c44b05bc7bc98a6d40ab059ad656dc78f5c4
Edfram-Guerra <edfram.guerra@gmail.com>
2024-07-26 22:13:48 -0300

    Exercico021 finalizado

---
+++ exercicio021/Genero.java
@@ -0,0 +1,23 @@
+package exercicio021;
+
+import java.util.Scanner;
+
+public class Genero {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Informe o sexo (M/F)");
+        char sexo = leitor.nextLine().charAt(0);
+
+        if (sexo == 'M' || sexo == 'm') {
+            System.out.println("Masculino");
+        }
+        if (sexo == 'F' || sexo == 'f') {
+            System.out.println("Feminino");
+        } else {
+            System.out.println("sexo invalido");
+        }
+        leitor.close();
+
+    }
+}
